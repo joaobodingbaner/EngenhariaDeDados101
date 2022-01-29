91548f7e3c3c0c3ed4f119b6f458f8e56c0ac759
thiagobeppe <thiagoabeppe@gmail.com>
2022-01-29 09:23:06 -0300

    update

--- exemplo_1/server.js
+++ exemplo_1/server.js
@@ -1,17 +1,30 @@
-'use strict'
+'use strict';
 
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser')
+const Pool = require('pg').Pool
 
-const PORT = 8080
-const HOST = '0.0.0.0'
-const app = express()
-
-app.get('/', (req,res)=>{
-    res.send('Hello World')
+// Constants
+const PORT = 8082;
+const HOST = '0.0.0.0';
+const pool = new Pool({
+  user: 'postgres',
+  host: 'db',
+  database: 'postgres',
+  password: 'lead',
+  port: 5432,
 })
 
-app.get('*', (req,res)=>{
-    res.send('iiih errou')
-})
 
-app.listen(PORT, HOST)
+
+// App
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+app.listen(PORT, HOST);
--- exemplo_2/init_scripts/init_teste.sql
+++ exemplo_2/init_scripts/init_teste.sql
@@ -1,9 +1,26 @@
+CREATE DATABASE metabase;
 CREATE SCHEMA treinamento_lead;
 
-CREATE TABLE IF NOT EXISTS treinamento_lead.data_engs(
+CREATE TABLE IF NOT EXISTS treinamento_lead.users(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(10),
     sobrenome VARCHAR(25)
 );
 
-INSERT INTO treinamento_lead.data_engs("nome", "sobrenome") VALUES ('thiago','beppe');
+CREATE TABLE IF NOT EXISTS treinamento_lead.aporte(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    cripto VARCHAR(25),
+    valor_do_aporte NUMERIC 
+);
+
+INSERT INTO treinamento_lead.users("nome", "sobrenome") VALUES ('Thiago','Beppe');
+INSERT INTO treinamento_lead.users("nome", "sobrenome") VALUES ('Lucas','Silva');
+INSERT INTO treinamento_lead.users("nome", "sobrenome") VALUES ('Priscila','Alcantara');
+INSERT INTO treinamento_lead.users("nome", "sobrenome") VALUES ('Whinderson','Nunes');
+INSERT INTO treinamento_lead.users("nome", "sobrenome") VALUES ('Arselino','Freitas');
+
+INSERT INTO treinamento_lead.aport("user_id","cripto","valor_do_aporte") VALUES ('1','Solana',100)
+INSERT INTO treinamento_lead.aport("user_id","cripto","valor_do_aporte") VALUES ('1','Bitcoin',500)
+INSERT INTO treinamento_lead.aport("user_id","cripto","valor_do_aporte") VALUES ('1','Bitcoin',0.10)
+INSERT INTO treinamento_lead.aport("user_id","cripto","valor_do_aporte") VALUES ('1','Solana',50)
